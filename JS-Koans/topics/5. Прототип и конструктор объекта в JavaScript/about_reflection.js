d219ecba713ad88e96e49a9863291e8f5cddf725
Mddu11 <vodilov_vano@mail.ru>
2025-11-10 17:39:58 +0300

    Задание 5 выполнено

--- JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_reflection.js
+++ JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_reflection.js
@@ -1,4 +1,4 @@
-describe("About Reflection (about_reflection.js)", function() {
+describe("About Reflection (about_reflection.js)", function() { 
   function A() {
     this.aprop = "A";
   };
@@ -16,13 +16,10 @@ describe("About Reflection (about_reflection.js)", function() {
     for (let propertyName in b) {
       keys.push(propertyName);
     }
-    // Сколько элементов в массиве keys?
-    expect(FILL_ME_IN).toBe(keys.length);
-    // Какие это элементы?
-    expect([FILL_ME_IN, FILL_ME_IN]).toEqual(keys);
 
-    // hasOwnProperty возвращает true, если параметр является свойством непосредственно объекта,
-    // но не в том случае, если это свойство доступно через цепочку прототипов.
+    expect(2).toBe(keys.length);
+    expect(["bprop", "aprop"]).toEqual(keys);
+
     let ownKeys = [];
     for(let propertyName in b) {
       if (b.hasOwnProperty(propertyName)) {
@@ -30,20 +27,16 @@ describe("About Reflection (about_reflection.js)", function() {
       }
     }
 
-    // Сколько элементов в массиве ownKeys?
-    expect(FILL_ME_IN).toBe(ownKeys.length);
-    // Какие это элементы?
-    expect([FILL_ME_IN]).toEqual(ownKeys);
+    expect(1).toBe(ownKeys.length);
+    expect(["bprop"]).toEqual(ownKeys);
   });
 
   it("constructor property", function () {
     let a = new A();
     let b = new B();
-    // Какой тип конструктора a?
-    expect(FILL_ME_IN).toBe(typeof a.constructor);
-    // Какое имя у конструктора a?
-    expect(FILL_ME_IN).toBe(a.constructor.name);
-    // Какое имя у конструктора b?
-    expect(FILL_ME_IN).toBe(b.constructor.name);
+
+    expect("function").toBe(typeof a.constructor);
+    expect("A").toBe(a.constructor.name);
+    expect("A").toBe(b.constructor.name);
   });
 });
