a24dd0d4d9b26ff49222a8e2186b63ed3437c528
Mddu11 <vodilov_vano@mail.ru>
2025-11-10 17:31:02 +0300

    Задание 5 выполнено

--- JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_prototypal_inheritance.js
+++ JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_prototypal_inheritance.js
@@ -1,9 +1,8 @@
 describe("About Prototypal Inheritance (about_prototypal_inheritance.js)", function() {
-  // Этот шаблон определяет класс по его конструктору
   let Mammal = function(name) {
     this.name = name;
   }
-  // То, что не нужно задавать в конструкторе, следует добавить в свойство прототипа конструктора.
+
   Mammal.prototype = {
     sayHi: function() {
       return "Hello, my name is " + this.name;
@@ -13,11 +12,9 @@ describe("About Prototypal Inheritance (about_prototypal_inheritance.js)", funct
   it("defining a 'class'", function() {
     let eric  = new Mammal("Eric");
 
-    // Что вернёт вызов метода?
-    expect(FILL_ME_IN).toBe(eric.sayHi());
+    expect("Hello, my name is Eric").toBe(eric.sayHi());
   });
 
-  // Добавим ещё один метод в класс Mammal, который использует метод sayHi.
   Mammal.prototype.favouriteSaying = function() {
     return this.name + "'s favourite saying is " + this.sayHi(); 
   }
@@ -25,8 +22,7 @@ describe("About Prototypal Inheritance (about_prototypal_inheritance.js)", funct
   it("more functions", function() {
     let bobby = new Mammal("Bobby");
 
-     // Что вернёт вызов метода?
-    expect(FILL_ME_IN).toBe(bobby.favouriteSaying()); 
+    expect("Bobby's favourite saying is Hello, my name is Bobby").toBe(bobby.favouriteSaying()); 
   });
 
   it("calling functions added to a prototype after an object was created", function() {
@@ -34,34 +30,25 @@ describe("About Prototypal Inheritance (about_prototypal_inheritance.js)", funct
     Mammal.prototype.numberOfLettersInName = function() {
       return this.name.length;
     };
-    // следующее действие просит объект paul вызвать метод, который был добавлен
-    // к прототипу Mammal после того, как paul был создан.
 
-     // Что вернёт вызов метода?
-    expect(FILL_ME_IN).toBe(paul.numberOfLettersInName());
+    expect(4).toBe(paul.numberOfLettersInName());
   });
 
-  // https://learn.javascript.ru/prototype-inheritance
   function extend(child, supertype){  
     child.prototype = supertype.prototype;  
   } 
 
-  // Дочерний класс Bat наследуется от родительского класса Mammal.
   function Bat(name, wingspan) {
     Mammal.call(this, name);
     this.wingspan = wingspan;
   }
 
-  // Попробуем наследование
   extend(Bat, Mammal);
 
   it("Inheritance", function() {
     let lenny = new Bat("Lenny", "1.5m");
 
-     // Что вернёт вызов метода?
-    expect(FILL_ME_IN).toBe(lenny.sayHi());
-
-    // Что находится в свойстве wingspan?
-    expect(FILL_ME_IN).toBe(lenny.wingspan);
+    expect("Hello, my name is Lenny").toBe(lenny.sayHi());
+    expect("1.5m").toBe(lenny.wingspan);
   });
 });
